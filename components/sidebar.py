b1c0e0f7bbf8d2890b7d51f40b4f59062aa1f71d
F Cesar Farias <fcesarf80@gmail.com>
2026-07-09 21:45:43 +0100

    refactor: gerar botões da Sidebar dinamicamente

--- components/sidebar.py
+++ components/sidebar.py
@@ -1,10 +1,23 @@
 import tkinter as tk
+
 from components.sidebar_button import SidebarButton
 
 
 class Sidebar(tk.Frame):
 
     WIDTH = 250
+
+    BUTTONS = [
+        "Dashboard",
+        "Livros",
+        "Utilizadores",
+        "Empréstimos",
+        "Devolução",
+        "Ativos",
+        "Histórico",
+        "CSV",
+        "Estatísticas",
+    ]
 
     def __init__(self, master):
 
@@ -20,12 +33,26 @@ class Sidebar(tk.Frame):
             relheight=1
         )
 
-        self.dashboard_button = SidebarButton(
-            self,
-            text="Dashboard"
-        )
+        self.buttons = []
 
-        self.dashboard_button.place(
-            x=15,
-            y=30
-        )
+        self.criar_botoes()
+
+    def criar_botoes(self):
+
+        y = 30
+
+        for texto in self.BUTTONS:
+
+            botao = SidebarButton(
+                self,
+                text=texto
+            )
+
+            botao.place(
+                x=15,
+                y=y
+            )
+
+            self.buttons.append(botao)
+
+            y += 55
